ac2574ac9cf29a8d2a8f838b5c989cc202887c48
GabrielGirotti <girottigabriel@gmail.com>
2024-06-25 14:28:22 -0300

    Creating React project with Vite, Typescript and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className=" text-center text-white font-bold">
+        Probando clases TailwindCss
+      </h1>
+    </>
+  );
+}
+
+export default App;
