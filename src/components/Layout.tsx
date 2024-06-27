ceaa4b9951d681a7c61956bf5523f50873751413
GabrielGirotti <girottigabriel@gmail.com>
2024-06-27 06:59:10 -0300

    starting user profile component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,11 @@
-
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div >
-        <h1 className="text-center text-white font-bold" > Aca va el Header </h1>
-        <br/>
-        <br/>
-        <Outlet />
-        <br/>
-        <br/>
-        <h1 className="text-center text-white font-bold" > Aca va el footer</h1>
+    <div>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
