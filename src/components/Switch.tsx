591ccde1ce8d2316d491b65ad6658a91159a2f05
GabrielGirotti <girottigabriel@gmail.com>
2024-06-30 12:51:03 -0300

    Switch component and conditional view

---
+++ src/components/Switch.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { ClasesProfile, CaloriesProfile } from "../components";
+
+export const Switch = () => {
+  const [show, setShow] = useState(false);
+
+  const handleSwitch = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      <div className=" flex flex-row bg-gray-300 p-[2px] rounded-3xl mt-8">
+        <button
+          onClick={() => handleSwitch()}
+          className={`w-28 h-8 font-lato rounded-2xl font-bold text-heading-sm duration-500 ${
+            !show ? "pointer-events-none bg-white" : " bg-gray-300"
+          }`}
+        >
+          Logros
+        </button>
+        <button
+          onClick={() => handleSwitch()}
+          className={`w-28 h-8 font-lato rounded-2xl font-bold text-heading-sm duration-500 ${
+            show ? "pointer-events-none bg-white" : " bg-gray-300"
+          }`}
+        >
+          Clases
+        </button>
+      </div>
+
+      {!show ? <CaloriesProfile /> : <ClasesProfile />}
+    </>
+  );
+};
+
+export default Switch;
